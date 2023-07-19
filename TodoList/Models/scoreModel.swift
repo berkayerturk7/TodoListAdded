46916e55dcd1625e99e99cc53b56c20bf42c9612
berkay <berkayert7@gmail.com>
2023-07-19 03:16:04 +0300

    Son değişiklikler

--- TodoList/Models/scoreModel.swift
+++ TodoList/Models/scoreModel.swift
@@ -7,12 +7,26 @@ struct ScoreModel: Identifiable, Codable, Hashable {
     let totalTasks: Int
     let doneTasks: Int
     
-    init(id: String = UUID().uuidString, dateScore: String, totalTasks: Int, doneTasks: Int) {
+    let titleOfTask: String
+    let completedOfTask: Bool
+  
+    let importanceOfTask: String
+    let rangeTime: String
+    
+    
+    init(id: String = UUID().uuidString, dateScore: String, totalTasks: Int, doneTasks: Int, titleOfTask: String, completedOfTask: Bool, importanceOfTask: String, rangeTime: String) {
         
         self.id = id
         self.dateScore = dateScore
         self.totalTasks = totalTasks
         self.doneTasks = doneTasks
+        
+        self.titleOfTask = titleOfTask
+        self.completedOfTask = completedOfTask
+        
+        self.importanceOfTask = importanceOfTask
+        
+        self.rangeTime = rangeTime
         
     }
     
